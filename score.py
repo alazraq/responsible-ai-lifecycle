0ea04e941238a49b66284edce63069c7746ab6c7
Abdou Lazraq <t-alazraq@microsoft.com>
2021-09-14 18:09:02 +0200

    Changes to Dockerfile and score.py

--- score.py
+++ score.py
@@ -1,5 +1,5 @@
 import joblib
-from azureml.core.model import Model
+import joblib
 import os
 import json
 import pandas as pd
@@ -7,17 +7,13 @@ import pandas as pd
 def init():
     # Create a global variable called model
     global model
-    # Load the model using the name of the model you registered
-    model_path = Model.get_model_path("catboost_predictor")
-    print("Model Path is  ", model_path)
     #Load the model from the path
-    model = joblib.load(model_path)
+    model = joblib.load(os.environ.get("MODEL_PATH")) 
 
 
 def run(request):
    try:
-     data = json.loads(request)
-     df = pd.read_json(data)
+     df = pd.read_json(request)
      result = model.predict(df)
      return {'data' : result.tolist() , 'message' : "Successfully classified loan"}
    except Exception as e:
